b49bedc04836fc0813ae80767fda14fe716c68bb
Avner José Guimarães Ribeiro <avnerjose@pop-os.localdomain>
2021-06-11 18:20:58 -0300

    Adding pwa feature

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-module.exports = {
+
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+    pwa: {
+        dest: 'public',
+        register: true
+    },
     images: {
         domains: ['storage.googleapis.com']
-    }
-}
+    },
+});
